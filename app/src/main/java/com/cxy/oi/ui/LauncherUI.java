195c267a4f8ab9d8d665501d6e684506bd7473d3
xingyuuchen <16602809965@163.com>
2020-12-11 00:07:34 +0800

    创建onCreate

--- app/src/main/java/com/cxy/oi/ui/LauncherUI.java
+++ app/src/main/java/com/cxy/oi/ui/LauncherUI.java
@@ -1,11 +1,27 @@
 package com.cxy.oi.ui;
 
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LauncherUI extends AppCompatActivity {
 
+    private static final String TAG = "LauncherUI";
 
 
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+    }
 
 
 }
