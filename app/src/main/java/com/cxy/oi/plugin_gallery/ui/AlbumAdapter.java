613a76a09a25b4b4b565c5a55a3bb5deb55f8a51
xingyuuchen <16602809965@163.com>
2020-12-14 17:18:38 +0800

    修改应用名

---
+++ app/src/main/java/com/cxy/oi/plugin_gallery/ui/AlbumAdapter.java
@@ -0,0 +1,29 @@
+package com.cxy.oi.plugin_gallery.ui;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class AlbumAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
