0b4e88ae3c6f657f4595875ddea59125da849b34
xingyuuchen <16602809965@163.com>
2021-01-14 23:27:34 +0800

    protobuff的autogen脚本 与 NetSceneQueryImg.proto

---
+++ scripts/autogen.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# Before you run this script, make sure you have installed ProtoBuffer.
+
+cd ..
+
+project_dir=$(pwd)
+
+cd app/autogen
+
+for file in `ls`
+do
+
+  if [ ${file##*.} == "proto" ]; then
+    echo "processing" ${file}" ..."
+
+    protoc -I=$(pwd) --java_out=${project_dir}/app/src/main/java \
+      $(pwd)/${file}
+  fi
+
+done
